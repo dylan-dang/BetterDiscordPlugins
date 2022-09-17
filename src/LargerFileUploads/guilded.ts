46c195cf06c7bb3fa92122fe851e3420997ce94c
dylan-dang <endercraft2319@gmail.com>
2022-09-16 21:52:04 -0500

    might give this up

--- src/LargerFileUploads/guilded.ts
+++ src/LargerFileUploads/guilded.ts
@@ -1,6 +1,6 @@
-import type { IncomingMessage, RequestOptions } from 'http';
-import type { Readable } from 'stream';
-import { deleteData, saveData } from 'bdapi';
+import { IncomingMessage, RequestOptions } from 'http';
+import { pipeline, Readable, ReadableOptions, Transform, Writable } from 'stream';
+import { deleteData, loadData, saveData } from 'bdapi';
 import https from 'https';
 
 interface Response extends Omit<IncomingMessage, keyof Readable | 'setTimeout'> {
@@ -35,21 +35,25 @@ function mediaEnpoint(path: string) {
     return new URL(path, 'https://media/guilded.gg/media');
 }
 
-function request(url: string | URL, { payload, ...options }: RequestOptions & { payload?: any }) {
+function request(url: string | URL, { payload, headers, ...options }: RequestOptions & { payload?: any }) {
     return new Promise<Response>((resolve, reject) => {
         let body = '';
-        const request = https.request(url, options, (response) => {
-            response.setEncoding('utf-8');
-            response.on('error', reject);
-            response.on('data', (chunk) => (body += chunk.toString()));
-            response.on('end', () => {
-                const cookies = response.headers['set-cookie']
-                    ?.map((cookie) => cookie?.match(/([^=]+)=([^\;]+);?/)?.[0])
-                    .join(' ');
-                if (cookies) saveData('cookies', cookies);
-                resolve({ ...response, response, body, json: JSON.parse(body) });
-            });
-        });
+        const request = https.request(
+            url,
+            { headers: { ...headers, Cookie: loadData('cookies') }, ...options },
+            (response) => {
+                response.setEncoding('utf-8');
+                response.on('error', reject);
+                response.on('data', (chunk) => (body += chunk.toString()));
+                response.on('end', () => {
+                    const cookies = response.headers['set-cookie']
+                        ?.map((cookie) => cookie?.match(/([^=]+)=([^\;]+);?/)?.[0])
+                        .join(' ');
+                    if (cookies) saveData('cookies', cookies);
+                    resolve({ ...response, response, body, json: JSON.parse(body) });
+                });
+            }
+        );
         request.on('error', reject);
         if (payload) request.write(payload);
         request.end();
@@ -89,6 +93,105 @@ export async function signup(email: string, password: string, name: string): Pro
     return statusCode === 200 ? json : await login(email, password);
 }
 
-export async function uploadMedia() {
-    return await post(mediaEnpoint('upload'),);
+interface Callbacks {
+    onProgress?(progress: number): void;
+    onResponse?(url: string): void;
+    onError?(error: NodeJS.ErrnoException | null): void;
+}
+
+export function uploadFile(
+    isMedia: boolean,
+    file: File,
+    { onProgress = () => {}, onResponse = () => {}, onError = () => {} }: Callbacks = {}
+) {
+    const endpoint = mediaEnpoint(isMedia ? 'upload' : 'file_upload');
+    const boundary = '---------------------------GamersRiseUp';
+
+    const stream = nodeStreamFrom(file.stream() as unknown as ReadableStream);
+    let bytesUploaded = 0;
+    let body = '';
+    const request = https.request(
+        endpoint,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': `multipart/form-data; name="file"; boundary=${boundary}`,
+                'Content-Length': file.size,
+                Accept: 'application/json',
+                Cookie: loadData('cookies'),
+            },
+        },
+        (response) => {
+            response.setEncoding('utf-8');
+            response.on('error', onError);
+            response.on('data', (chunk) => (body += chunk.toString()));
+            response.on('end', () => {
+                const cookies = response.headers['set-cookie']
+                    ?.map((cookie) => cookie?.match(/([^=]+)=([^\;]+);?/)?.[0])
+                    .join(' ');
+                if (cookies) saveData('cookies', cookies);
+                onResponse(JSON.parse(body).url);
+            });
+        }
+    );
+
+    request.write(boundary);
+    request.write('\n');
+    request.write(`Content-Disposition: form-data; name="file"; filename="${file.name}";\n`);
+    request.write(`Content-Type: ${file.type}\n\n`);
+
+    pipeline(
+        stream,
+        new Transform({
+            transform(chunk, encoding, callback) {
+                bytesUploaded += chunk.length;
+                this.push(chunk);
+                onProgress(bytesUploaded / file.size);
+                callback();
+            },
+            flush(callback) {
+                this.write(`\n\n${boundary}--`);
+                callback();
+            },
+        }),
+        request,
+        onError
+    );
+}
+
+function nodeStreamFrom(readableStream: ReadableStream, { read, destroy, ...options }: ReadableOptions = {}) {
+    const reader = readableStream.getReader();
+    let closed = false;
+
+    const readable = new Readable({
+        ...options,
+        read(size) {
+            read?.call(this, size);
+            reader.read().then((chunk) => {
+                readable.push(chunk.done ? null : chunk.value);
+            }, readable.destroy);
+        },
+        destroy(error, callback) {
+            destroy?.call(this, error, callback);
+            const done = () => callback(error);
+            if (!closed) {
+                reader.cancel(error).then(done, done);
+                return;
+            }
+            done();
+        },
+    });
+
+    reader.closed.then(
+        () => {
+            closed = true;
+            if (!readable.readableEnded) readable.push(null);
+        },
+        (error) => {
+            closed = true;
+            readable.destroy(error);
+        }
+    );
+
+    return readable;
 }
--- src/LargerFileUploads/index.tsx
+++ src/LargerFileUploads/index.tsx
@@ -2,14 +2,65 @@ import { Patcher } from 'bdapi';
 import { getModule } from 'bdapi/webpack';
 import { byProps } from 'bdapi/webpack/filters';
 import { snowflake } from 'discord/types';
+import type EventEmitter from 'events';
+import https from 'https';
+import { uploadFile } from './guilded';
 
 const MAX_SIZE = 524288000; // 500 MB
 
-declare class CloudUploadClass {
-    upload(): Promise<void>;
+enum CloudUploadStatus {
+    NOT_STARTED = 'NOT_STARTED',
+    STARTED = 'STARTED',
+    UPLOADING = 'UPLOADING',
+    ERROR = 'ERROR',
+    COMPLETED = 'COMPLETED',
+    CANCELED = 'CANCELED',
 }
 
-const { CloudUpload }: { CloudUpload: typeof CloudUploadClass } = getModule(byProps('CloudUpload'));
+declare class CloudUploadClass extends EventEmitter {
+    channelId: snowflake;
+    classification: string;
+    description: string | null;
+    filename: string;
+    id: string;
+    isImage: boolean;
+    isVideo: boolean;
+    item: {file: File, platform: number};
+    loaded: number;
+    reactNativeFileIndex: number;
+    responseUrl: string;
+    showLargeMessageDialog: boolean;
+    size: number;
+    spoiler: boolean;
+    status: CloudUploadStatus;
+    uploadedFilename: string;
+    _abortController: AbortController;
+    _aborted: boolean;
+    error: any;
+
+    isPatched?: boolean;
+
+    static fromJson(): CloudUploadClass;
+    retryOpts(): {
+        timeout: number,
+        backoff: number,
+        retries: number,
+    };
+    uploadFileToCloud(): Promise<any>;
+    upload(): Promise<void>;
+    reactNativeCompressAndExtractData(): Promise<void>;
+    handleError(error: any): void;
+    handleComplete(e: any): void;
+    cancel(): void;
+    resetState(): void;
+    delete(): Promise<void>;
+    setResponseUrl(url: string): void;
+    setStatus(status: CloudUploadStatus): void;
+    setFilename(filename: string): void;
+    setUploadedFilename(filename: string): void;
+}
+
+const { CloudUpload }: { CloudUpload: typeof CloudUploadClass, CloudUploadStatus: typeof CloudUploadStatus } = getModule(byProps('CloudUpload'));
 
 type FileClassification =
     | 'photoshop'
@@ -43,6 +94,23 @@ interface FileHandler {
 
 const FileHandler: FileHandler = getModule(byProps('anyFileTooLarge'));
 
+interface UploadFilesOptions {
+    channelId: snowflake;
+    uploads: CloudUploadClass[];
+    draftType: number;
+    parsedMessage: {
+        content: string;
+        invalidEmojis: any[];
+        tts: boolean;
+        validNonShortcutEmojis: any[];
+    };
+    options: {
+        stickerIds: snowflake[];
+    }
+}
+
+const FileUploader: { uploadFiles(options: UploadFilesOptions): void } = getModule(m => m.default.uploadFiles);
+
 export function start() {
     const loggedIn = true;
 
@@ -52,9 +120,22 @@ export function start() {
     Patcher.instead(FileHandler, 'uploadSumTooLarge', (_, [files], uploadSumTooLarge) =>
         loggedIn ? false : uploadSumTooLarge(files)
     );
-    Patcher.before(CloudUpload.prototype, 'upload', (self: typeof CloudUpload.prototype) => {
-        console.log(self);
+    Patcher.instead(CloudUpload.prototype, 'upload', async (self: typeof CloudUpload.prototype) => {
+        uploadFile(self.isImage || self.isVideo, self.item.file);
+        self.setResponseUrl("https://discord-attachments-uploads-prd.storage.googleapis.com/c2cee40b-fb43-40b2-955c-e83af2b6676b/PXL_20210224_003501840.jpeg?upload_id=ADPycdtwUwf4ztoY-6pIzL03lByXqvSLvPJOBhGRflsnixVMf24Gu9yA-CqfrgVcaXL26ueNi7qkUfWo4Aq1m845zRx3noMPJ2kd");
+        self.setUploadedFilename("c2cee40b-fb43-40b2-955c-e83af2b6676b/PXL_20210224_003501840.jpeg");
+        self.handleComplete({
+            body: null,
+            headers: {},
+            ok: true,
+            status: 200,
+            text: ""
+        })
     });
+
+    Patcher.before(FileUploader, 'uploadFiles', (self, [opts]) => {
+        console.log(opts);
+    })
 }
 
 export function stop() {
